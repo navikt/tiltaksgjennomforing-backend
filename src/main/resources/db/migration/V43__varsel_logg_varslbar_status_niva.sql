dae1b6a526d25ecd3ba86942d68fe96b7ae5f747
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-02-04 11:35:23 +0100

    renamet bjellevarsel table til varsel
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>
    Co-authored-by: Millad Dagdoni <millad.dagdoni@nav.no>

--- src/main/resources/db/migration/V43__varsel_logg_varslbar_status_niva.sql
+++ src/main/resources/db/migration/V43__varsel_logg_varslbar_status_niva.sql
@@ -2,3 +2,4 @@ alter table bjelle_varsel add column varslbar_status varchar;
 alter table hendelselogg add column beskrivelse varchar;
 alter table hendelselogg add column hendelse_status varchar;
 
+alter table bjelle_varsel rename to varsel
